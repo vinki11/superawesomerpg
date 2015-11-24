8fd67fa2d15bb7d71604b158753d68e54fbb88d0
Vincent <gelinas.vincent@gmail.com>
2015-11-24 16:54:50 -0500

    Affichage du groupeAventurier dans menuJeu

--- WindowsFormsApplication1/menuJeu.cs
+++ WindowsFormsApplication1/menuJeu.cs
@@ -22,14 +22,63 @@ namespace JRPG
 
         private void afficherGroupeAventurier()
         {
-           txtNom1.Text = Program.groupeAventurier.Membres[0].NomAventurier;
-           txtLvl1.Text = Program.groupeAventurier.Membres[0].Niveau.ToString();
-           txtXP1.Text = Program.groupeAventurier.Membres[0].Experience.ToString();
-        }
+            txtNom1.Visible = false;
+            lblLvl1.Visible = false;
+            txtLvl1.Visible = false;
+            lblXP1.Visible = false;
+            txtXP1.Visible = false;
+            txtNom2.Visible = false;
+            lblLvl2.Visible = false;
+            txtLvl2.Visible = false;
+            lblXP2.Visible = false;
+            txtXP2.Visible = false;
+            txtNom3.Visible = false;
+            lblLvl3.Visible = false;
+            txtLvl3.Visible = false;
+            lblXP3.Visible = false;
+            txtXP3.Visible = false;
+
+            if (Program.groupeAventurier.Membres.Count >= 1)
+            {
+                txtNom1.Visible = true;
+                lblLvl2.Visible = true;
+                txtLvl1.Visible = true;
+                lblXP1.Visible = true;
+                txtXP1.Visible = true;
+                txtNom1.Text = Program.groupeAventurier.Membres[0].NomAventurier;
+                txtLvl1.Text = Program.groupeAventurier.Membres[0].Niveau.ToString();
+                txtXP1.Text = Program.groupeAventurier.Membres[0].Experience.ToString();
+            }
+
+            if (Program.groupeAventurier.Membres.Count >= 2)
+            {
+                txtNom2.Visible = true;
+                lblLvl2.Visible = true;
+                txtLvl2.Visible = true;
+                lblXP2.Visible = true;
+                txtXP2.Visible = true;
+                txtNom2.Text = Program.groupeAventurier.Membres[1].NomAventurier;
+                txtLvl2.Text = Program.groupeAventurier.Membres[1].Niveau.ToString();
+                txtXP2.Text = Program.groupeAventurier.Membres[1].Experience.ToString();
+            }
+
+            if (Program.groupeAventurier.Membres.Count >= 3)
+            {
+                txtNom3.Visible = true;
+                lblLvl3.Visible = true;
+                txtLvl3.Visible = true;
+                lblXP3.Visible = true;
+                txtXP3.Visible = true;
+                txtNom3.Text = Program.groupeAventurier.Membres[2].NomAventurier;
+                txtLvl3.Text = Program.groupeAventurier.Membres[2].Niveau.ToString();
+                txtXP3.Text = Program.groupeAventurier.Membres[2].Experience.ToString();
+            }
+}
 
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             Application.Exit();
         }
+        
     }
 }
